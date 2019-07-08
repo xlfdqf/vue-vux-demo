a99abd535164dc634c238ce0849adc30895ccd87
xulanfang <1574425015@qq.com>
2019-07-08 15:54:50 +0800

    创建项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <x-header></x-header>
+  </div>
+</template>
+
+<script>
+import { XHeader } from "vux";
+export default {
+  name: "HelloWorld",
+  components: {
+    XHeader
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
